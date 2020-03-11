25afea6c003c758f5d9eb4cc7988a20129015f4c
Alexandr Morozov <mopoz07@gmail.com>
2020-03-11 15:10:12 +0200

    crash course

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <h1>About page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam labore repudiandae et modi consequuntur ratione.</p>
+    <p>Ipsam voluptas sunt explicabo! Nulla excepturi enim corrupti quibusdam sunt consequuntur! Possimus repellendus totam assumenda.</p>
+    <p>Quisquam itaque, repellendus quos et eos dolorem aut soluta? A minima sit nobis alias quam.</p>
+  </section>
+  
+</template>
+
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
+
+<style>
+
+</style>template 
